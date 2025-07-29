740573245aea4e7af0a4448488e0ac43a9fd681c
MohammadFalahFront <mohammadfalahfront@gmail.com>
2025-07-29 16:16:21 +0300

    build

---
+++ dist/assets/VaPageNotFoundLargeText-18434ef8.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./VaPageNotFound.vue_vue_type_style_index_0_lang-087b50f4.js";import{T as a,o as t,c as o,i as s,f as _}from"./index-b794012a.js";const n=_("div",{class:"va-page-not-found-large-text__number"},"404",-1),d=a({setup(r){return(c,i)=>(t(),o(e,{class:"va-page-not-found-large-text"},{image:s(()=>[n]),_:1}))}});export{d as default};
